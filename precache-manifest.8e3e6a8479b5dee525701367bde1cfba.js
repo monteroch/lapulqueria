4b658db22abe0298800ad757f38711d930330722
Christian Montero Martínez <christianmontero08@gmail.com>
2020-06-21 18:45:57 -0700

    Updates

---
+++ precache-manifest.8e3e6a8479b5dee525701367bde1cfba.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d2ad9ea6444f9146df59bf0c0efc6f",
+    "url": "/lapulqueria/index.html"
+  },
+  {
+    "revision": "141c9135480f7b5c5711",
+    "url": "/lapulqueria/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0df72f56ce719154e8dc",
+    "url": "/lapulqueria/static/js/2.68fff086.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/lapulqueria/static/js/2.68fff086.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "141c9135480f7b5c5711",
+    "url": "/lapulqueria/static/js/main.d332a28e.chunk.js"
+  },
+  {
+    "revision": "52582773c957a51184f9",
+    "url": "/lapulqueria/static/js/runtime-main.92ec27ae.js"
+  },
+  {
+    "revision": "ecd737eb0f09b1421f11806f6166c615",
+    "url": "/lapulqueria/static/media/doge.ecd737eb.png"
+  },
+  {
+    "revision": "09626a174378e91e645fd13a66ea2acd",
+    "url": "/lapulqueria/static/media/jarLogo.09626a17.png"
+  },
+  {
+    "revision": "c4ef1f0e6c874c5b7cf7ff7b42c63ef0",
+    "url": "/lapulqueria/static/media/pulqueTwoJars.c4ef1f0e.jpg"
+  },
+  {
+    "revision": "30eb25ab24976e316a869356106519d6",
+    "url": "/lapulqueria/static/media/pulqueria_logo.30eb25ab.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7c2d2c95a36b051efcdf33f63e5af439.js"
+  "/lapulqueria/precache-manifest.8e3e6a8479b5dee525701367bde1cfba.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lapulqueria/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
