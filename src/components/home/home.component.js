3ec9c515b8da6debb004c8d7db79fd5c01fc86bf
Fredomontero <christianmontero08@gmail.com>
2020-08-08 23:44:14 -0700

    Remove unused imports in home page

--- src/components/home/home.component.js
+++ src/components/home/home.component.js
@@ -12,7 +12,6 @@ import ourpulques from '../../assets/images/ourpulques.jpg';
 import craftbeer from '../../assets/images/craftbeer.jpg'
 
 //Components
-// import Section from '../section/section.component';
 import BlockImage from '../blockImage/blockImage.component';
 
 
