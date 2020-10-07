46d515b20ef4acd8fd0dd18535b747587a1f52ac
Fredomontero <christianmonntero08@gmail.com>
2020-10-06 22:22:21 -0700

    Feat: setup event component

---
+++ src/components/event/event.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import useStyles from './event.styles';
+import { Grid, Typography } from '@material-ui/core'
+
+export const Event = () => {
+
+  return(
+    <Grid>
+      <Typography>This is the event component</Typography>
+    </Grid>
+  )
+
+}
